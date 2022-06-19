ef6bd6f5a298d4b73d8125d0edb297c55875a892
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-06-19 07:48:43 +0300

    feat: alternative syntax of field 'data'

--- app.js
+++ app.js
@@ -1,42 +1,11 @@
-const App = {
-  data() {
-    return {
-      placeholderString: 'Введите название заметки',
-      title: 'Список заметок',
-      inputValue: '',
-      notes: ['Заметка 1', 'Заметка 2', 'Заметка 3']
-    }
-  },
-  methods: {
-    addHewNote() {
-      if (this.inputValue) {
-        this.notes.push(this.inputValue);
-        this.inputValue = '';
-      }
-    },
-    toUpperCase(item) {
-      return item.toUpperCase();
-    },
-    deleteNote(idx) {
-      this.notes = this.notes.filter((item, i) => i !== idx);
-    }
-  },
-  computed: { // all methods must return value and mast have modify variables. these are getters, setters
-    doubleCountComputed() {
-      console.log('doubleCountComputed')
-      return this.notes.length * 2;
-    },
-  },
-  watch: {
-    inputValue(value) { // watch for field inputValue
-      if (value.length > 10) {
-        this.inputValue = '';
-      }
-    }
-  }
-};
+Vue.createApp({
+  // data() {
+  //   return {
+  //
+  //   }
+  // }
 
-
-
-Vue.createApp(App)
-.mount('#app');
+  data: () => ({
+    title: 'Я есть Грут'
+  })
+}).mount('#app');
