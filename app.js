d9398b0e5a3157960286bdf9486b1cc35db00c5c
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-06-19 19:50:26 +0300

    feat: use virtual DOM for optimization render

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+const h = Vue.h;
+
 const app = Vue.createApp({
   data() {
     return {
       title: 'Это из свойства template'
     }
   },
-  template: `
-    <div class="card center">
-      <h1>{{ title }}</h1>
-      <button class="btn" @click="title = 'Изменили!'">Изменить</button>
-    </div>
-  `
+  methods: {
+    changeTitle() {
+      this.title = 'Изменили!'
+    }
+  },
+  // template: `
+  //   <div class="card center">
+  //     <h1>{{ title }}</h1>
+  //     <button class="btn" @click="title = 'Изменили!'">Изменить</button>
+  //   </div>
+  // `
+  render() {
+    return h('div', {
+      class: 'card center'
+    },
+      [
+        h('h1', {}, this.title),
+        h('button', {
+          class: 'btn',
+          onClick: this.changeTitle
+        }, 'Изменить')
+      ])
+  }
 })
 
 app.mount('#app');
