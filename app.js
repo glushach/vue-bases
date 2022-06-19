72e432261044de790f698180db097a2b5989386f
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-06-19 08:36:03 +0300

    feat: additional possible of v-for

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ Vue.createApp({
 
   data: () => ({
     myHtml: '<h1>Vue 3 App</h1>',
-    title: 'Я есть Грут'
+    title: 'Я есть Грут',
+    person: {
+      firstName: 'Alex',
+      lastName: 'Solomonic',
+      age: 27
+    },
+    items: [1, 2, 3, 4, 5]
   })
 }).mount('#app');
