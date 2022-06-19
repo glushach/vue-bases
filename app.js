3aa0e80dfd302a18778d90f806ad87cf183b193e
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-06-19 19:10:08 +0300

    feat: render html from js

--- app.js
+++ app.js
@@ -1,36 +1,15 @@
-Vue.createApp({
-  // data() {
-  //   return {
-  //
-  //   }
-  // }
-
-  data: () => ({
-    myHtml: '<h1>Vue 3 App</h1>',
-    title: 'Я есть Грут',
-    person: {
-      firstName: 'Alex',
-      lastName: 'Solomonic',
-      age: 27
-    },
-    items: [1, 2]
-  }),
-  methods: {
-    addItem(event) {
-      this.items.unshift(this.$refs.myInput.value);
-      this.$refs.myInput.value = '';
-      console.log(event.key)
-    },
-    remove(i) {
-      this.items.splice(i, 1);
-    },
-    log(item) {
-      console.log('Log item: ', item)
+const app = Vue.createApp({
+  data() {
+    return {
+      title: 'Это из свойства template'
     }
   },
-  computed: {
-    eventItems() {
-      return this.items.filter(i => i % 2 === 0);
-    }
-  }
-}).mount('#app');
+  template: `
+    <div class="card center">
+      <h1>{{ title }}</h1>
+      <button class="btn" @click="title = 'Изменили!'">Изменить</button>
+    </div>
+  `
+})
+
+app.mount('#app');
