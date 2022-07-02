8012686004f18ef9bdd1d01900d6496a9637f110
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-07-02 09:54:17 +0300

    feat: generate card's news in tag template (inside file App.js)

--- vue-framework/src/App.vue
+++ vue-framework/src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
-    <h1>This is title</h1>
-    <the-header></the-header>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>Астуальные новости {{now}}</h2>
+    </div>
+    
+    <div class="card" v-for="item in news" :key="item">
+      <h3>{{item}}</h3>
+      <button class="btn" @click="isOpen = !isOpen">Открыть</button>
+      <p v-if="isOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolore, facere. Amet animi eligendi eos eveniet excepturi, facilis fugit id libero magni omnis provident quae quibusdam reprehenderit saepe, ut, veniam.</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
+  data() {
+    return {
+      now: new Date().toLocaleDateString(),
+      isOpen: false,
+      news: [
+        'Джо Банден победили на выборах в США',
+        'Vue 3 успешно работает'
+      ]
+    }
   }
 }
 </script>
