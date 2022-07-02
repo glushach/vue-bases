cb63affea9061f453ba6a2fcf5f74dd4a5a45c05
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-07-02 16:57:47 +0300

    feat: use slots for App.vue and AppBlock.vue

--- vue-framework/src/App.vue
+++ vue-framework/src/App.vue
@@ -1,79 +1,31 @@
 <template>
   <div class="container pt-1">
     <div class="card">
-      <h2>Астуальные новости {{now}}</h2>
-      <span>Открыто: <strong>{{ openRate }}</strong> | Прочитано: <strong>{{ readRate }}</strong></span>
+      <h2>Slots</h2>
     </div>
-    
-    <app-news
-        v-for="item in news"
-        :key="item.id"
-        :title="item.title"
-        :id="item.id"
-        :is-open="item.isOpen"
-        :was-read="item.wasRead"
-        @open-news="openNews"
-        @read-news="readNews"
-        @unmark="unreadNews"
-    ></app-news>
+    <app-block>
+      <p>Это смый важный текст для нового блока</p>
+      
+      <template #header>
+        <h3>Это заголовок!</h3>
+      </template>
+      
+      <template v-slot:footer>
+        <hr />
+        <small>Это footer</small>
+      </template>
+    </app-block>
   </div>
 </template>
 
 
 <script>
-
-import AppNews from './AppNews'
+import AppBlock from "./AppBlock";
 export default {
-  data() {
-    return {
-      now: new Date().toLocaleDateString(),
-      openRate: 0,
-      readRate: 0,
-      news: [
-        {
-          title: 'Джо Банден победили на выборах в США',
-          id: 1,
-          isOpen: false,
-          wasRead: false
-        },
-        {
-          title: 'Vue 3 успешно работает',
-          id: 2,
-          isOpen: false,
-          wasRead: false
-        }
-      ]
-    }
-  },
-  provide() {
-    return {
-      title: 'Список всех новостей',
-      news: this.news
-    }
-  },
-  methods: {
-    openNews() {
-      this.openRate++
-    },
-    readNews(id) {
-      const idx = this.news.findIndex(news => news.id === id)
-      this.news[idx].wasRead = true
-      this.readRate++
-    },
-    unreadNews(id) {
-      const news = this.news.find(news => news.id === id)
-      news.wasRead = false
-      this.readRate--
-    }
-  },
-  components: {
-    'app-news': AppNews
-  }
+  components: {AppBlock}
 }
 </script>
 
 <style scoped>
-  h2 {
-    color: darkred;
-  }
+
 </style>
