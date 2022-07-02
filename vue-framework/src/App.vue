346ff6e8545e4eae09925024b15233f8100abbb8
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-07-02 09:17:24 +0300

    feat: start project 06-vue-components

--- vue-framework/src/App.vue
+++ vue-framework/src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <h1>Hello Vue 3!</h1>
+  <div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+  }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+
 </style>
