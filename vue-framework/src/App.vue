eb1cdf3abd91c2925d94b57f9243429a67dba312
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-07-03 14:50:53 +0300

    init: start project with forms

--- vue-framework/src/App.vue
+++ vue-framework/src/App.vue
@@ -1,67 +1,60 @@
 <template>
-  <div class="container pt-1">
-    <div class="card">
-      <app-async-component></app-async-component>
-      <h2>Динамические и асинхронные компоненты</h2>
-      
-      <app-button
-          ref="myBtn"
-          :color="oneColor"
-          @action="active = 'one'"
-      >One</app-button>
-      <app-button
-          :color="twoColor"
-          @action="active = 'two'"
-      >Two</app-button>
-    </div>
-    
-    <keep-alive>
-      <component :is="componentName"></component>
-    </keep-alive>
+  <div class="container">
+    <form class="card">
+      <h1>Анкета на Vue разработчика!</h1>
+      <div class="form-control">
+        <label for="name">Как тебя зовут?</label>
+        <input type="text" id="name" placeholder="Введи имя">
+      </div>
+
+      <div class="form-control">
+        <label for="age">Выбери возраст</label>
+        <input type="number" id="age" value="20">
+      </div>
+
+      <div class="form-control">
+        <label for="city">Твой город</label>
+        <select id="city">
+          <option value="spb">Санкт-Петербург</option>
+          <option value="msk">Москва</option>
+          <option value="kzn">Казань</option>
+          <option selected value="nsk">Новосибирск</option>
+        </select>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Готов к переезду в Токио?</span>
+        <div class="checkbox">
+          <label><input type="radio" name="trip"/> Да</label>
+        </div>
+
+        <div class="checkbox">
+          <label><input type="radio" name="trip"/> Нет</label>
+        </div>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Что знаешь во Vue?</span>
+        <div class="checkbox">
+          <label><input type="checkbox"/> Vuex</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox"/> Vue CLI</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox"/> Vue Router</label>
+        </div>
+      </div>
+
+      <button type="submit" class="btn primary">Отправить</button>
+    </form>
   </div>
 </template>
 
-
 <script>
-  import AppButton from "./AppButton";
-  import AppTextOne from "./AppTextOne";
-  import AppTextTwo from "./AppTextTwo";
-  import AppAsyncComponent from "./AppAsyncComponent";
-  export default {
-    data() {
-      return {
-        active: 'one' // two
-      }
-    },
-    mounted() {
-      // setTimeout(() => {
-      //   this.componentName = 'new comp name'
-      // }, 1500)
-      this.$refs.myBtn.btnLog()
-    },
-    computed: {
-/*      componentName() {
-        return 'app-text-' + this.active
-      },*/
-      componentName: {
-        get() {
-          return 'app-text-' + this.active
-        },
-        set(value) {
-          console.log('computedName', value)
-        }
-      },
-      oneColor() {
-        return this.active === 'one' ? 'primary' : ''
-      },
-      twoColor() {
-        return this.active === 'two' ? 'primary' : ''
-      },
-    },
-    components: {AppAsyncComponent, AppButton, AppTextOne, AppTextTwo }
-  }
+  export default {}
 </script>
 
-<style scoped>
+<style>
 
 </style>
--- vue-framework/src/main.js
+++ vue-framework/src/main.js
@@ -1,15 +1,5 @@
-import { createApp, defineAsyncComponent } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import TheHeader from './TheHeader'
 import './theme.css'
 
-// App -> AppNews -> AppNewsList
-// App -> AppNewsList
-const app = createApp(App)
-
-// global
-app.component('the-header', TheHeader)
-app.component('async-component', defineAsyncComponent(() => {
-  return import('./AppAsyncComponent')
-}))
-app.mount('#app')
+createApp(App).mount('#app')
