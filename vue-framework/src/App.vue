b63886e7f490ed67b7d3f02353923d3ab7522e9c
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-13 21:11:31 +0300

    feat: start work with http

--- vue-framework/src/App.vue
+++ vue-framework/src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="container">
-    <div class="card">
+    <form class="card" @submit.prevent="createPerson">
       <h2>Работа с базой данних</h2>
-    </div>
+      
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+      
+      <button class="btn primary"
+              :disabled="name.length === 0"
+      >Создать человека</button>
+    </form>
   </div>
 </template>
 
 
 <script>
 export default {
-
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    createPerson() {
+      // this.name
+    }
+  },
+  
 }
 </script>
 
