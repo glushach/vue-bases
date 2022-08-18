44ca778cef417972cc7517518a3abac6dc66cf7e
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-18 14:54:50 +0300

    feat: use modal with teleport

--- vue-framework/src/App.vue
+++ vue-framework/src/App.vue
@@ -4,23 +4,38 @@
       <h2>{{$i18n('app.title')}}</h2>
       
       <button class="btn" @click="changeLang">{{ $i18n('app.changeBtn')}}</button>
+      <button class="btn primary" @click="modal = true">Open modal</button>
+      
+      <teleport to="#modal">
+        <app-modal
+            v-if="modal"
+            @close="modal = false"
+        ></app-modal>
+      </teleport>
+      
     </div>
   </div>
 </template>
 
 
 <script>
-
+import AppModal from "./components/AppModal";
 export default {
   mounted() {
   },
   inject: ['changeI18N'],
+  data() {
+    return {
+      modal: false
+    }
+  },
   methods: {
     changeLang() {
       this.changeI18N('en');
       this.$forceUpdate();
     }
-  }
+  },
+  components: { AppModal }
 }
 </script>
 
---
+++ vue-framework/src/components/AppModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="modal-backdrop" @click="$emit('close')"></div>
+  <div class="modal">
+    <h3>This is title of modal</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid amet animi, aperiam culpa cumque ipsum rem sint suscipit veniam.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    emits: ['close']
+  }
+</script>
+
+<style scoped>
+
+</style>
