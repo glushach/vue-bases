00eb51c546aed1a61a885c27809e2c9fe8202cc9
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-27 13:00:47 +0300

    feat: use local storage and global storage

--- vue-framework/src/App.vue
+++ vue-framework/src/App.vue
@@ -1,15 +1,29 @@
 <template>
+  <the-navbar></the-navbar>
   <div class="container with-nav">
     <div class="card">
       <h1>Про Vuex</h1>
-      <h2>Счетчик 0</h2>
+      <h2>Счетчик {{$store.state.counter}}</h2>
+      <button class="btn" @click="increment">Add</button>
     </div>
   </div>
+<!--  <button @click="increment">Increase</button> &lt;!&ndash; it is local state because it works inside component&ndash;&gt;-->
 </template>
 
 <script>
 import TheNavbar from './TheNavbar'
 export default {
-  components: {TheNavbar}
+  components: {TheNavbar},
+  // data() {
+  //   return {
+  //     counter: 0
+  //   }
+  // },
+  methods: {
+    increment() {
+      // this.counter++;
+      this.$store.state.counter++
+    }
+  }
 }
 </script>
