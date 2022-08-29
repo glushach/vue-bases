8dd3f2297e1eb8d5fb9257648f293678287844f1
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-29 11:01:10 +0300

    feat: compare ref and reactive

--- vue-framework/src/App.vue
+++ vue-framework/src/App.vue
@@ -6,42 +6,55 @@
       <hr>
       <p>Название: <strong>{{ name }}</strong></p>
       <p>Версия: <strong>{{ version }}</strong></p>
-
+      
       <button class="btn" @click="changeInfo">Изменить</button>
     </div>
   </div>
 </template>
 
 <script>
-  import { ref } from 'vue';
-  
-export default {
-  setup() {
-    const name = ref('VueJS')
-    const version = ref(3)
-    
-    function changeInfo() {
-      name.value = 'Vue JS!'
-      version.value = 4
-    }
-    
-    return {
-      name,
-      version,
-      changeInfo
-    }
-  },
-  // data() {
-  //   return {
-  //     name: 'VueJS',
-  //     version: 3
-  //   }
-  // },
-  // methods: {
-  //   changeInfo() {
-  //     this.name = 'Vue JS!'
-  //     this.version = 4
-  //   }
-  // }
-}
+  import {ref, reactive, toRefs, isRef, isReactive} from 'vue';
+
+  export default {
+    setup() {
+      const name = ref('VueJS')
+      const version = ref(3)
+
+      console.log(isRef(name)) // true
+      console.log(isRef(version.value)) // false
+
+      const framework = reactive({
+        name: 'Vue',
+        version: 3
+      })
+
+      // console.log(isReactive(framework)) // true
+      // console.log(isReactive(framework.name)) // false
+
+      function changeInfo() {
+        name.value = 'Vue JS!'
+        version.value = 42
+        // framework.name = 'Vue JS!'
+        // framework.version = 42
+      }
+
+      return {
+        name,
+        version,
+        changeInfo
+      }
+    },
+    // data() {
+    //   return {
+    //     name: 'VueJS',
+    //     version: 3
+    //   }
+    // },
+    // methods: {
+    //   changeInfo() {
+    //     this.name = 'Vue JS!'
+    //     this.version = 4
+    //   }
+    // }
+  }
 </script>
