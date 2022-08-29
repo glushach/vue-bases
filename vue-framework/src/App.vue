2ec7e8279062007bc8a35988b267271a6a818913
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-29 11:43:19 +0300

    feat: use v-model

--- vue-framework/src/App.vue
+++ vue-framework/src/App.vue
@@ -8,7 +8,8 @@
       <p>Версия: <strong>{{ version }} ({{doubleVersion}})</strong></p>
       
       <div class="form-control">
-        <input type="text" ref="textInput">
+<!--        <input type="text" ref="textInput">-->
+        <input type="text" v-model="firstName">
       </div>
       
       <button class="btn" @click="changeInfo">Изменить</button>
@@ -32,23 +33,27 @@
       const name = ref('VueJS')
       const version = ref(3)
       const textInput = ref(null)
+      const firstName = ref('')
 
       const doubleVersion = computed(() => {
         return version.value * 2
       })
       
-      watch([doubleVersion, name], (newValues, oldValues) => {
-        console.log('new version', newValues[0])
-        console.log('new', newValues[1])
-        console.log('old version', oldValues[0])
-        console.log('old name', oldValues[1])
-      });
+      // watch([doubleVersion, name], (newValues, oldValues) => {
+      //   console.log('new version', newValues[0])
+      //   console.log('new', newValues[1])
+      //   console.log('old version', oldValues[0])
+      //   console.log('old name', oldValues[1])
+      // });
+      watch(firstName, (newV, oldV) => {
+        console.log(newV)
+        // console.log(oldV)
+      })
 
       function changeInfo() {
         name.value = 'Vue JS!'
         version.value = 42
 
-        console.log(textInput.value.value)
       }
 
       return {
@@ -56,7 +61,8 @@
         version,
         changeInfo,
         doubleVersion,
-        textInput
+        textInput,
+        firstName
       }
     },
     // data() {
