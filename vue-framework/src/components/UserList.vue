b9cb59eae18fabe5ffc02bb7a039f750e19b8834
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-30 19:04:58 +0300

    feat: get data from service

--- vue-framework/src/components/UserList.vue
+++ vue-framework/src/components/UserList.vue
@@ -11,12 +11,12 @@
 
 <script lang="ts">
   import {defineComponent} from 'vue'
-  
+  import {useUsers} from "@/use/users";
+
   export default defineComponent({
-    setup() {
-      return {
-        users: []
-      }
+    async setup() {
+      const {users} = await useUsers()
+      return {users}
     }
   })
 </script>
---
+++ vue-framework/src/use/users.ts
@@ -0,0 +1,16 @@
+import {ref, Ref} from 'vue'
+import {useFetch} from "@/use/fetch";
+
+export async function useUsers(): Promise<any> {
+  const loading: Ref<boolean> = ref(false)
+  const {response: users, request} = useFetch(
+    'https://jsonplaceholder.typicode.com/users'
+  )
+
+  if (!loading.value) {
+    await request()
+    loading.value = true
+  }
+
+  return {users}
+}
