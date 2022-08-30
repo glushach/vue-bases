ad9b0f94c044171ba3c2b8b2295e2498cde2dd2a
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-30 18:38:59 +0300

    feat: example typing usable hooks

--- vue-framework/src/views/Users.vue
+++ vue-framework/src/views/Users.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="card">
     <h1>Пользователи</h1>
+    
+    
   </div>
 </template>
+
+<script lang="ts">
+  import {defineComponent} from 'vue'
+  import UserList from "@/components/UserList.vue";
+  
+  export default defineComponent({
+    components: {UserList}
+  })
+</script>
